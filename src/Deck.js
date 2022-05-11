b9c1dee01703611f2644362350b3312153ee398f
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-05-11 16:09:58 -0600

    feat: add Deck class, add cards property to Deck class, wrote intial tests for Deck class

---
+++ src/Deck.js
@@ -0,0 +1,8 @@
+class Deck {
+  constructor(cards) {
+    this.cards = cards
+    console.log(this.cards, 'line 4');
+  }
+}
+
+module.exports = Deck;
--- test/Turns-test.js
+++ test/Turns-test.js
@@ -7,22 +7,26 @@ describe('Turns', () => {
 
   it('should be a function', () => {
     const turn = new Turns()
+
     expect(Turns).to.be.a('function');
   });
 
   it('should be an instance of turn', () => {
     const turn = new Turns()
+
     expect(turn).to.be.an.instanceof(Turns);
   });
   
   it("should store a user's guess to a question", () => {
     const turn = new Turns('prototype method')
+
     expect(turn.guess).to.equal('prototype method');
   });
 
   it('should recognize current card in play', () => {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
     const turn = new Turns('array', card)
+    
     expect(turn.card).to.be.an.instanceof(Card);
   });
 
